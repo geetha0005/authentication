8982acdd60b7f5ee8f51bfafcfb3155a1b318c31
Geetha Chegonda <geetha.chegonda@zf.com>
2025-09-18 06:16:02 +0530

    Added private route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { SignUpPage } from './SignUpPage';
 import { LogInPage } from './LogInPage';
 import { UserInfoPage } from './UserInfoPage';
-
+import {PrivateRoute} from "./PrivateRoute"
 function App() {
   return (
     <div className="page-container">
@@ -11,7 +11,9 @@ function App() {
         <Routes>
           <Route path="/log-in" element={<LogInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
-          <Route path="/" element={<UserInfoPage />} />
+          <Route element={<PrivateRoute redirectPath="log-in" isAllowed={true}/>}>
+           <Route path="/" element={<UserInfoPage />} />
+        </Route>
         </Routes>
       </BrowserRouter>
     </div>
